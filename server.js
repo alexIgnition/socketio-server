ad3a49fa4eec8b3faac0eab2f7d337e7763993b9
Alex Perjescu <alex.perjescu@ignitiononline.com>
2023-05-25 15:31:22 +0100

    port setting change

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-import express from 'express';
-import { createServer as createHTTPServer } from 'http';
-import { Server as socketIOServer } from 'socket.io';
-const cors = require('cors'); // Add this line
+import express from "express";
+import { createServer as createHTTPServer } from "http";
+import { Server as socketIOServer } from "socket.io";
+import cors from "cors";
 
 const app = express();
-app.use(cors()); // Add this line
+app.use(cors());
 
 const httpServer = createHTTPServer(app);
-const io = new socketIOServer(httpServer);
+const io = new socketIOServer(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'], // Add any additional HTTP methods if needed
+  },
+});
 
 // Socket.IO event handling
-io.on('connection', (socket) => {
-  console.log('New client connected');
+io.on("connection", (socket) => {
+  console.log("New client connected");
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 
   // Handle location updates from clients
-  socket.on('locationUpdate', (data) => {
+  socket.on("locationUpdate", (data) => {
     // Broadcast the location update to all connected clients
-    io.emit('locationUpdate', data);
+    io.emit("locationUpdate", data);
   });
 });
 
 // Start the server
 const port = 3001;
-httpServer.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+httpServer.listen(port);
